e4adfba0e9a1bc425c2cae5f39fa6a8f9fe49ac0
pcboy <david.hagege@gmail.com>
2020-12-31 13:21:30 +0900

    Update URL and add github banner.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # bread-baking-calculator
 
-Access the baker's percentage calculator at: https://pcboy.github.io/bread-baking-calculator/
+Access the baker's percentage calculator at: https://breadcalc.joynetiks.com/
 
+Everything underlined in the UI can be freely edited.  
+The state of the calculator is saved in the URL, so you can just bookmark the full URL of your recipes and the calculator will restore the state on load.
 
 <img src="./assets/screenshot.png" width="90%" />
--- components/Calculator/index.tsx
+++ components/Calculator/index.tsx
@@ -16,6 +16,7 @@ import { Flours } from "./Flours";
 import { SCalculator } from "./SCalculator";
 import { handleChangeNumber, stripHTML } from "./Utils";
 import ContentEditable from "react-contenteditable";
+import GithubCorner from "react-github-corner";
 
 const Calculator = observer(() => {
   React.useEffect(() => calculatorStore.loadHash(), []);
@@ -27,87 +28,98 @@ const Calculator = observer(() => {
     );
 
   return (
-    <div className="container" style={{ padding: "1rem" }}>
-      <SCalculator>
-        <h1>
-          <ContentEditable
-            html={calculatorStore.recipeName}
-            className="editable"
-            disabled={false}
-            onChange={(e) => {
-              calculatorStore.changeAttribute("recipeName", e.target.value);
-            }}
-          />
-        </h1>
-        <div className="columns is-multiline is-mobile">
-          <div className="column is-half">Final Dough Weight</div>
-          <div className="column is-half">
-            <Input
-              type="number"
-              style={{ width: "100%" }}
-              value={calculatorStore.totalWeight}
-              onClick={(e) => e.target?.select()}
-              onChange={(e) => handleChangeNumber(e, "totalWeight")}
-              endAdornment={<InputAdornment position="end">g</InputAdornment>}
+    <>
+      <GithubCorner
+        href={"https://github.com/pcboy/bread-baking-calculator"}
+        bannerColor="#151513"
+        octoColor="#fff"
+        size={80}
+        direction="right"
+      />
+      <div className="container" style={{ padding: "1rem" }}>
+        <SCalculator>
+          <h1>
+            <ContentEditable
+              html={calculatorStore.recipeName}
+              className="editable"
+              disabled={false}
+              onChange={(e) => {
+                calculatorStore.changeAttribute("recipeName", e.target.value);
+              }}
             />
+          </h1>
+          <div className="columns is-multiline is-mobile">
+            <div className="column is-half">Final Dough Weight</div>
+            <div className="column is-half">
+              <Input
+                type="number"
+                style={{ width: "100%" }}
+                value={calculatorStore.totalWeight}
+                onClick={(e) => e.target?.select()}
+                onChange={(e) => handleChangeNumber(e, "totalWeight")}
+                endAdornment={<InputAdornment position="end">g</InputAdornment>}
+              />
+            </div>
           </div>
-        </div>
-        <Flours />
+          <Flours />
 
-        <div className="columns is-multiline is-mobile">
-          <div className="column is-half">Water</div>
-          <div className="column is-half">
-            <Input
-              type="number"
-              style={{ width: "100%" }}
-              value={calculatorStore.waterPerc}
-              onClick={(e) => e.target?.select()}
-              onChange={(e) => handleChangeNumber(e, "waterPerc")}
-              endAdornment={<InputAdornment position="end">%</InputAdornment>}
-            />
+          <div className="columns is-multiline is-mobile">
+            <div className="column is-half">Water</div>
+            <div className="column is-half">
+              <Input
+                type="number"
+                style={{ width: "100%" }}
+                value={calculatorStore.waterPerc}
+                onClick={(e) => e.target?.select()}
+                onChange={(e) => handleChangeNumber(e, "waterPerc")}
+                endAdornment={<InputAdornment position="end">%</InputAdornment>}
+              />
 
-            <div className="weight">{waterWeight()} grams</div>
+              <div className="weight">{waterWeight()} grams</div>
+            </div>
           </div>
-        </div>
 
-        <Ingredients />
+          <Ingredients />
 
-        <div className="columns is-multiline is-mobile">
-          <div className="column is-half">
-            Starter in percentage of total weight (100% hydration starter)
+          <div className="columns is-multiline is-mobile">
+            <div className="column is-half">
+              Starter in percentage of total weight (100% hydration starter)
+            </div>
+            <div className="column is-half">
+              <Input
+                type="number"
+                style={{ width: "100%" }}
+                value={calculatorStore.starterPerc}
+                onClick={(e) => e.target?.select()}
+                onChange={(e) => handleChangeNumber(e, "starterPerc")}
+                endAdornment={<InputAdornment position="end">%</InputAdornment>}
+              />
+              <div className="weight">
+                {calculatorStore.starterWeight} grams
+              </div>
+              <FormControl style={{ width: "100%" }}>
+                <InputLabel htmlFor="age-native-simple">
+                  Which Flour did you use?
+                </InputLabel>
+                <Select
+                  native
+                  value={calculatorStore.starterFlourIndex}
+                  onChange={(e: React.ChangeEvent<any>) => {
+                    calculatorStore.starterFlour(parseInt(e.target.value));
+                  }}
+                >
+                  {calculatorStore.flours.map((flour, index: number) => (
+                    <option key={`flour_${index}`} value={index}>
+                      {stripHTML(flour.name)}
+                    </option>
+                  ))}
+                </Select>
+              </FormControl>
+            </div>
           </div>
-          <div className="column is-half">
-            <Input
-              type="number"
-              style={{ width: "100%" }}
-              value={calculatorStore.starterPerc}
-              onClick={(e) => e.target?.select()}
-              onChange={(e) => handleChangeNumber(e, "starterPerc")}
-              endAdornment={<InputAdornment position="end">%</InputAdornment>}
-            />
-            <div className="weight">{calculatorStore.starterWeight} grams</div>
-            <FormControl style={{ width: "100%" }}>
-              <InputLabel htmlFor="age-native-simple">
-                Which Flour did you use?
-              </InputLabel>
-              <Select
-                native
-                value={calculatorStore.starterFlourIndex}
-                onChange={(e: React.ChangeEvent<any>) => {
-                  calculatorStore.starterFlour(parseInt(e.target.value));
-                }}
-              >
-                {calculatorStore.flours.map((flour, index: number) => (
-                  <option key={`flour_${index}`} value={index}>
-                    {stripHTML(flour.name)}
-                  </option>
-                ))}
-              </Select>
-            </FormControl>
-          </div>
-        </div>
-      </SCalculator>
-    </div>
+        </SCalculator>
+      </div>
+    </>
   );
 });
 
