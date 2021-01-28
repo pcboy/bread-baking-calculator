85eb113ecaab71f6eb3b1798b8b1c888ed7428f7
pcboy <david.hagege@gmail.com>
2021-01-28 20:58:16 +0900

    Fix build

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Calculator from "./components/Calculator";
 import { CalcStoreContext } from "./stores/CalculatorStore";
-import "bulma/bulma.sass";
 
 const App = ({ store }: { store: any }) => (
   <CalcStoreContext.Provider value={store}>
--- entrypoint.tsx
+++ entrypoint.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import calculatorStore from "./stores/CalculatorStore";
+import "bulma/bulma.sass";
 
 ReactDOM.render(
   <App store={calculatorStore} />,
